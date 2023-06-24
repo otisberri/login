139ff1b4bc6d1d98bb4fab37e814ec81c0b95c90
Otis Berri <otisberri@gmail.com>
2023-06-24 04:21:33 +0000

    My first React project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-function App() {
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // You can add your own logic for authentication, API calls, etc.
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="container">
+
+      <div className="l-container">
+        <ul>
+            <li>About</li>
+            <li>Privacy</li>
+            <li>Terms of Use</li>
+            <li>FAQ</li>
+        </ul>
+      </div>
+
+      <div className="r-container">
+        <p>Login to your<br/> student dashboard</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="USER ID"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+          <input
+            type="password"
+            placeholder="PASSWORD"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+          <div className='base'>
+            <input type="submit" value="Login" />
+            <a href="#">Forgot User ID or Password</a>
+          </div>
+          
+        </form>
+      </div>
+
     </div>
   );
 }
 
-export default App;
+export default LoginPage;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+// import App from './App';
+import LoginPage from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<LoginPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
